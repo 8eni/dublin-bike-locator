0c8ab35db4bab0b966d7c294d37178a56b4eef23
Admin <bennirussell@gmail.com>
2019-02-21 08:18:28 +0000

    Updates App.js with new flow

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from './views/Map';
+import axios from 'axios';
+import { environment } from 'environment/environment';
+import BottomNav from './components/BottomNav';
+import CircularIndeterminate from './components/CircularIndeterminate';
+import StationDetails from './components/StationDetails';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#64ffda',
+    },
+    secondary: {
+      main: '#263238',
+    },
+  },
+});
 class App extends Component {
 
   constructor(){
     super();
+    this.dbBaseUrl = environment.db_base_url;
+    this.dbContract = environment.db_contract;
+    this.dbApiKey = environment.db_api_key;
+    this.getStations = this.getStations.bind(this)
     this.state = {
       center: {}, 
-      zoom: 16
+      zoom: 16,
+      stations: []
     }
   }
 
   componentDidMount() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
+        this.getStations(position.coords.latitude, position.coords.longitude)
         this.setState({
           center: {
             lat: position.coords.latitude,
@@ -25,10 +47,80 @@ class App extends Component {
     }
   }
 
+  getStations(lat, lng) {
+    axios.get(`${this.dbBaseUrl}stations?contract=${this.dbContract}&apiKey=${this.dbApiKey}`)
+      .then(res => {
+        const result = res.data.map(item => {
+          item.distance = this.distance(
+            lat,
+            lng,
+            item.position.lat, item.position.lng, 'K').toFixed(2);
+            return item;
+        })
+        this.setState({ 
+          stations: this.getClosest(result)
+        })
+      });
+  }
+
+  getClosest(stations) {
+    return stations.sort((a,b) => (a.distance > b.distance) ? 1 : ((b.distance > a.distance) ? -1 : 0))
+    // debugger
+    // return newt[0];
+  }
+
+
+  distance(lat1, lon1, lat2, lon2, unit) {
+    if ((lat1 === lat2) && (lon1 === lon2)) {
+      return 0;
+    }
+    else {
+      var radlat1 = Math.PI * lat1/180;
+      var radlat2 = Math.PI * lat2/180;
+      var theta = lon1-lon2;
+      var radtheta = Math.PI * theta/180;
+      var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+      if (dist > 1) {
+        dist = 1;
+      }
+      dist = Math.acos(dist);
+      dist = dist * 180/Math.PI;
+      dist = dist * 60 * 1.1515;
+      if (unit==="K") { dist = dist * 1.609344 }
+      if (unit==="N") { dist = dist * 0.8684 }
+      return dist;
+    }
+  }
+
   render() {
-    return (
-      <Map center={ this.state.center } zoom={ this.state.zoom }/>
-    );
+    if (this.state.stations.length) {
+      return (
+        <MuiThemeProvider theme={theme}>
+          { this.state.stations.map((station, i) =>
+
+            <StationDetails
+            key={ i }
+            stationDetails={ station } />
+            // <Marker
+            //   key={ i }
+            //   viewStation={ this.updateStation }
+            //   lat={ station.position.lat }
+            //   lng={ station.position.lng }
+            //   station={ station } />
+          )}
+          {/* <Map stations={ this.state.stations } center={ this.state.center } zoom={ this.state.zoom }/> */}
+          <BottomNav />
+        </MuiThemeProvider>
+      );
+    } else {
+      return (
+      <MuiThemeProvider theme={theme}>
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <CircularIndeterminate></CircularIndeterminate>
+      </div>
+      </MuiThemeProvider>
+      )
+    }
   }
 }
 
--- src/components/StationDetails.js
+++ src/components/StationDetails.js
@@ -1,16 +1,78 @@
-import React, { Component } from 'react';
-class StationDetails extends Component {
+// import React from 'react';
+// import Button from '@material-ui/core/Button';
 
-  render() {
-    const {
-      name,
-      available_bikes: bikes,
-      available_bike_stands: stands,
-      distance
-    } = this.props.stationDetails;
-    return (name) ?     
-      (<div className="station">{ name } Stands - { stands } Bikes - { bikes } - { distance }</div>) :
-      (<></>)
-  }
+// const StationDetails = ({ stationDetails }) => (stationDetails) ?
+  // <div>
+  //   { stationDetails.name } Stands - { stationDetails.available_bikes } 
+  //   Bikes - { stationDetails.available_bike_stands } - { stationDetails.distance }
+  //   <Button variant="contained" color="primary">
+  //     Hello Worlds
+  //   </Button>
+  // </div> :
+//   <div>none</div>
+
+// export default StationDetails;
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    margin: 'auto',
+    maxWidth: 500,
+  },
+  image: {
+    width: 128,
+    height: 128,
+  },
+  img: {
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
+});
+
+function StationDetails(props) {
+  const { classes, stationDetails } = props;
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid container spacing={16}>
+          <Grid item>
+            {/* <ButtonBase className={classes.image}>
+              <img className={classes.img} alt="complex" src="/static/images/grid/complex.jpg" />
+            </ButtonBase> */}
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={16}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1">{ stationDetails.address }</Typography>
+                <Typography gutterBottom>{ stationDetails.last_update}</Typography>
+                <Typography color="textSecondary">BIKES: { stationDetails.available_bikes } | SPACES: { stationDetails.available_bike_stands }</Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Typography variant="subtitle1">{ stationDetails.distance }Km</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
 }
-export default StationDetails;
+
+StationDetails.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(StationDetails);
